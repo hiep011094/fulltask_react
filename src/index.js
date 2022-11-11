846f942eeaeb3c86fe560cae26b463e64634d035
hiep011094 <hiep011094@gmail.com>
2022-11-11 12:49:29 +0700

    hiep

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import DataProvider from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <DataProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </ DataProvider>    
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
